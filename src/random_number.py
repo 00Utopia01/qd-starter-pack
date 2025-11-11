db1eab0d1cc6d0771a622dce7702a4702af264d1
00Utopia01 <germenaedoardo@gmail.com>
2025-11-11 18:47:09 +0100

    feat: add random number generator with get request using an API

--- src/random_number.py
+++ src/random_number.py
@@ -4,3 +4,9 @@
 #   Output:
 #   The random number is: 4
 #
+
+import requests
+
+response = requests.get('https://www.random.org/integers/?num=5&min=1&max=6&col=1&base=10&format=plain&rnd=new')
+
+print(f"random number is:{(response.text.strip())[0]}")
